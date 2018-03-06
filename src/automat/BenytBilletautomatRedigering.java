d61dbfc858b27e19a96a3b8e605ad565a054e17a
Robert <Robert@DESKTOP-9ODFEVG.eduroam.wireless.dtu.dk>
2018-03-06 16:30:59 +0100

    MenuSystem 0.1
    Baseline for new menu system

--- src/automat/BenytBilletautomatRedigering.java
+++ src/automat/BenytBilletautomatRedigering.java
@@ -18,27 +18,218 @@ public class BenytBilletautomatRedigering
             System.out.println("VELKOMMEN TIL BT BILLETAUTOMAT");
             System.out.println();
             System.out.println();
-            System.out.println("Tast 1 for at købe billet");
+            System.out.println("Tast 1 | Køb billet");
             System.out.println();
             
             if (automat.erMontør()) {
-                System.out.println("Tast 11 for at se status (montør)");
-                System.out.println("Tast 12 for at nulstille (montør)");
-                System.out.println("Tast 13 for at sætte billetpris (montør)");
-                System.out.println("Tast 14 for at sætte zone (montør)");
-                System.out.println("Tast 15 for udskrivning af log (montør)");
-                System.out.println("Tast 16 for at logge ud af montørtilstand");
+                System.out.println("Tast 11 | Se status (montør)");
+                System.out.println("Tast 12 | Nulstil (montør)");
+                System.out.println("Tast 13 | Sæt billetpris (montør)");
+                System.out.println("Tast 14 | Sæt zone (montør)");
+                System.out.println("Tast 15 | Udskriv log (montør)");
+                System.out.println("Tast 16 | Log ud af montørtilstand");
             }
             int start = tastatur.nextInt();
             tastatur.nextLine();
             
             switch (start){
                 case 1:
-                    System.out.println("Vælg billettype:");
+                    
+                    int personType;
+                    
+                    do
+                    {
+                    System.out.println("Tast 1 | Voksen (16+)");
+                    System.out.println("Tast 2 | Børn");
+                    System.out.println("Tast 3 | Studerende");
+                    System.out.println("Tast 4 | Pensionist");
+                    System.out.println("Tast 5 | Hund");
+                    System.out.println("Tast 6 | Cykel");
+                    personType = tastatur.nextInt();
+                    switch(personType){
+                        case 1:                        
+                        System.out.println("Voksenbillet (16+) valgt");
+                        // Indsæt funktion for tilføjelse af voksenbillet
+                        break;
+                        
+                        case 2:                        
+                        System.out.println("Børnebillet valgt");
+                        // Indsæt funktion for tilføjelse af børnebillet
+                        break;
+                        
+                        case 3:
+                        System.out.println("Billet til studerende valgt (husk gyldig studiekort)");
+                        // Indsæt funktion for tilføjelse af studerende
+                        break;
+                        
+                        case 4:
+                        System.out.println("Pensionistbillet valgt");
+                        // Indsæt funktion for tilføjelse af penseionist
+                        break;
+                        
+                        case 5:
+                        System.out.println("Hundebillet valgt");
+                        // Indsæt funktion for tilføjelse af penseionist
+                        break;
+                        
+                        case 6:
+                        System.out.println("Cykelbillet valgt");
+                        // Indsæt funktion for tilføjelse af penseionist
+                        break;
+                    
+                        default:
+                            personType = 0;
+                            break;
+                    }
+                    } while (personType == 0);
                     
                     
+                            
+                    int zoneType;
+
+                    do
+                    {
+                    System.out.println("Tast 1 | Vælg antal zoner");
+                    System.out.println("Tast 2 | Udregn antal zoner");
+                    zoneType = tastatur.nextInt();
+                    switch(zoneType){
+                        case 1:                     
+                        int zoneValg;
                     
+                            do
+                            {
+                            System.out.println("Tast 1 | 1 zone");
+                            System.out.println("Tast 2 | 2 zoner");
+                            System.out.println("Tast 3 | 3 zoner");
+                            System.out.println("Tast 4 | 4 zoner");
+                            System.out.println("Tast 5 | 5 zoner");
+                            System.out.println("Tast 6 | 6 zoner");
+                            zoneValg = tastatur.nextInt();
+                            switch(zoneValg){
+                                case 1:                        
+                                System.out.println("1 zone valgt");
+                                // Indsæt funktion for tilføjelse af 1 zone 
+                                break;
+
+                                case 2:                        
+                                System.out.println("2 zoner valgt");
+                                // Indsæt funktion for tilføjelse af 2 zoner
+                                break;
+
+                                case 3:
+                                System.out.println("3 zoner valgt");
+                                // Indsæt funktion for tilføjelse af 3 zoner
+                                break;
+
+                                case 4:
+                                System.out.println("4 zoner valgt");
+                                // Indsæt funktion for tilføjelse af 4 zoner
+                                break;
+
+                                case 5:
+                                System.out.println("5 zoner valgt");
+                                // Indsæt funktion for tilføjelse af 5 zoner
+                                break;
+
+                                case 6:
+                                System.out.println("6 zoner valgt");
+                                // Indsæt funktion for tilføjelse af 6 zoner
+                                break;
+
+                                default:
+                                    zoneValg = 0;
+                                    break;
+                            }
+                            } while (zoneValg == 0);  
+                        break;
+                        
+                        case 2:                        
+                        System.out.println("Hvilken zone ender rejsen i?");
+                        int slutZone = tastatur.nextInt();
+                        // automat.zoneBeregner(slutZone);
+                        break;                                           
                     
+                        default:
+                            zoneType = 0;
+                            break;
+                    }
+                    } while (zoneType == 0);
+                    
+                    System.out.println("Vælg antal billetter:");
+                    int antalBilletter = tastatur.nextInt();
+                    
+                    // INDKØBSKURV
+                    System.out.println("Indkøbskurv er nu: bla bla bla");
+                    
+                    int godkendBillet;
+                    
+                    do
+                    {
+                    System.out.println("Tast 1 | Godkend og gå til betaling");
+                    System.out.println("Tast 2 | Køb flere billetter");                   
+                    godkendBillet = tastatur.nextInt();
+                    switch(godkendBillet){
+                        case 1:                        
+                        {                    
+                        int betalingsMetode;
+                        boolean godkendtBetaling=false;
+                        
+                        while (godkendtBetaling == false)
+                        {
+                        do
+                        {
+                        System.out.println("Betal med:");
+                        System.out.println("Tast 1 | Dankort");
+                        System.out.println("Tast 2 | Kontant");
+                        System.out.println("Tast 3 | Kupon");
+                        betalingsMetode = tastatur.nextInt();
+                        switch(betalingsMetode){
+                            case 1:                        
+                            System.out.println("Indsæt dankort og tast kode:");
+                            System.out.println("...");
+                            System.out.println("Godkendt");  
+                            break;
+
+                            case 2:                        
+                            System.out.println("Klar til modtagelse af kontanter");
+                            break;
+
+                            case 3:
+                            System.out.print("Indtast kuponkode: ");
+                            //int kuponKode = tastatur.next();
+                            //automat.checkKuponKode
+                            break;
+
+                            default:
+                                betalingsMetode = 0;
+                                break;
+                        }
+                        } while (betalingsMetode == 0);
+                        System.out.print("Skriv beløb: ");
+                        int beløb = tastatur.nextInt();
+                        automat.indsætPenge(beløb);
+                        
+                        if (automat.checkBalance() == true){
+                        godkendtBetaling = true;
+                        automat.udskrivBillet();
+                        } else {
+                        System.out.println("Balance på " + automat.getBalance() + " kr. for lav, indbetal flere penge");
+                        }
+                        }
+                        
+                        break;
+                        }
+                        
+                        case 2:                        
+                        System.out.println("Returner til hovedmenu");
+                        // Indsæt funktion for tilføjelse af børnebillet
+                        break;                 
+                    
+                        default:
+                            godkendBillet = 0;
+                            break;
+                    }
+                    } while (godkendBillet == 0);
                     
                     break;
                     
