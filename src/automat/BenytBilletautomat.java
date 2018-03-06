b543bc83e976b12fab6f8cac0789d34c7e373cfd
Robert <Robert@DESKTOP-9ODFEVG.eduroam.wireless.dtu.dk>
2018-03-06 11:58:17 +0100

    PaymentOption 0.1.2
    Still working on it

--- src/automat/BenytBilletautomat.java
+++ src/automat/BenytBilletautomat.java
@@ -5,107 +5,107 @@ package automat;
 
 public class BenytBilletautomat
 {
-	public static void main(String[] arg)
-	{
-		Billetautomat automat = new Billetautomat();
-		java.util.Scanner tastatur = new java.util.Scanner(System.in);  // forbered
+    public static void main(String[] arg)
+    {
+        Billetautomat automat = new Billetautomat();
+        java.util.Scanner tastatur = new java.util.Scanner(System.in);  // forbered
 
-		System.out.println("BenytBilletautomat version 3");
-		System.out.println();
-		
-		while (true) {
-			System.out.println("-----------------------------------------------");
-			System.out.println("En billet koster " + automat.getBilletpris() + " kroner");
-			System.out.println("Balancen er på " + automat.getBalance() + " kroner");
-			System.out.println();
-			System.out.println("Tast 1 for at indbetale penge");
-			System.out.println("Tast 2 for at udskrive din billet");
-			System.out.println("Tast 3 for at få returpengene");
-			System.out.println();
-			System.out.println("Tast 10 for at logge ind som montør");
-			if (automat.erMontør()) {
-				System.out.println("Tast 11 for at se status (montør)");
-				System.out.println("Tast 12 for at nulstille (montør)");
-				System.out.println("Tast 13 for at sætte billetpris (montør)");
-				System.out.println("Tast 14 for udskrivning af log (montør)");
-                                System.out.println("Tast 15 for at logge ud af montørtilstand");
-			}
-			int valg = tastatur.nextInt();
-			tastatur.nextLine();
-		
-                    switch (valg) {
-                        case 1:
+        System.out.println("BenytBilletautomat version 3");
+        System.out.println();
+
+        while (true) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("En billet koster " + automat.getBilletpris() + " kroner");
+            System.out.println("Balancen er på " + automat.getBalance() + " kroner");
+            System.out.println();
+            System.out.println("Tast 1 for at indbetale penge");
+            System.out.println("Tast 2 for at udskrive din billet");
+            System.out.println("Tast 3 for at få returpengene");
+            System.out.println();
+            System.out.println("Tast 10 for at logge ind som montør");
+            if (automat.erMontør()) {
+                System.out.println("Tast 11 for at se status (montør)");
+                System.out.println("Tast 12 for at nulstille (montør)");
+                System.out.println("Tast 13 for at sætte billetpris (montør)");
+                System.out.println("Tast 14 for udskrivning af log (montør)");
+                System.out.println("Tast 15 for at logge ud af montørtilstand");
+            }
+            int valg = tastatur.nextInt();
+            tastatur.nextLine();
+
+            switch (valg) {
+                case 1:
+                    {
+                        System.out.println("Tast 1 for Dankort");
+                        System.out.println("Tast 2 for Kontant");
+                        System.out.println("Tast 3 for Kupon");
+                        int betalingsMetode = tastatur.nextInt();
+
+                        switch(betalingsMetode){
+                            case 1:
                             {
-                                System.out.println("Tast 1 for Dankort");
-                                System.out.println("Tast 2 for Kontant");
-                                System.out.println("Tast 3 for Kupon");
-                                int betalingsMetode = tastatur.nextInt();
-                                
-                                switch(betalingsMetode){
-                                    case 1:
-                                    {
-                                        System.out.println("Indsæt kort og tast kode:");
-                                        System.out.println("...");
-                                        System.out.println("Godkendt");
-                                    }
-                                    case 2:
-                                    {
-                                        System.out.println("Klar til modtagelse af kontanter");
-                                    }
-                                    case 3:
-                                    {
-                                        System.out.print("Indtast kuponkode: ");
-                                        //int kuponKode = tastatur.
-                                    }
-                                }
-                                System.out.print("Tast 1 for indsættelse med DankortSkriv beløb: ");
-                                int beløb = tastatur.nextInt();
-                                automat.indsætPenge(beløb);
-                                break;
+                                System.out.println("Indsæt kort og tast kode:");
+                                System.out.println("...");
+                                System.out.println("Godkendt");
                             }
-                        case 2:
-                            if (automat.checkBalance() == true){
-                                automat.udskrivBillet();
-                            } else {
-                                System.out.println("Balance for lav, indbetal penge");
-                            }
-                            break;
-                        case 3:
+                            case 2:
                             {
-                                int beløb = automat.returpenge();
-                                System.out.println("Du fik "+beløb+" retur retur");
-                                break;
+                                System.out.println("Klar til modtagelse af kontanter");
                             }
-                        case 10:
-                            System.out.print("Skriv kode: ");
-                            String kode = tastatur.next();
-                            automat.montørLogin(kode);
-                            break;
-                        case 11:
-                            System.out.println("Antal billetter solgt: " + automat.getAntalBilletterSolgt());
-                            System.out.println("Total indkomst: " + automat.getTotal()+" kr");
-                            break;
-                        case 12:
-                            automat.nulstil();
-                            break;
-                        case 13:
+                            case 3:
                             {
-                                System.out.print("Skriv beløb: ");
-                                int beløb = tastatur.nextInt();
-                                automat.setBilletpris(beløb);
-                                break;
+                                System.out.print("Indtast kuponkode: ");
+                                //int kuponKode = tastatur.
                             }
-                        case 14:
-                            automat.getLog();
-                            break;
-                        case 15:
-                            automat.montørLogin("");
-                            ;
-                            break;
-                        default:
-                            System.out.println("Ugyldigt valg, prøv igen");
-                            break;
+                        }
+                        System.out.print("Tast 1 for indsættelse med DankortSkriv beløb: ");
+                        int beløb = tastatur.nextInt();
+                        automat.indsætPenge(beløb);
+                        break;
                     }
-		}
-	}
+                case 2:
+                    if (automat.checkBalance() == true){
+                        automat.udskrivBillet();
+                    } else {
+                        System.out.println("Balance for lav, indbetal penge");
+                    }
+                    break;
+                case 3:
+                    {
+                        int beløb = automat.returpenge();
+                        System.out.println("Du fik "+beløb+" retur retur");
+                        break;
+                    }
+                case 10:
+                    System.out.print("Skriv kode: ");
+                    String kode = tastatur.next();
+                    automat.montørLogin(kode);
+                    break;
+                case 11:
+                    System.out.println("Antal billetter solgt: " + automat.getAntalBilletterSolgt());
+                    System.out.println("Total indkomst: " + automat.getTotal()+" kr");
+                    break;
+                case 12:
+                    automat.nulstil();
+                    break;
+                case 13:
+                    {
+                        System.out.print("Skriv beløb: ");
+                        int beløb = tastatur.nextInt();
+                        automat.setBilletpris(beløb);
+                        break;
+                    }
+                case 14:
+                    automat.getLog();
+                    break;
+                case 15:
+                    automat.montørLogin("");
+                    
+                    break;
+                default:
+                    System.out.println("Ugyldigt valg, prøv igen");
+                    break;
+            }
+        }
+    }
 }
